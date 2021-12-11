6983d7fb068fc34e2101809be948d21ce9db8d2b
hfykc7 <hfykc7@nottingham.edu.my>
2021-12-12 00:31:13 +0800

    Move to MainClass package

--- src/main/java/MainClass/GraphicsMain.java
+++ src/main/java/MainClass/GraphicsMain.java
@@ -15,7 +15,9 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package test;
+package MainClass;
+
+import test.GameFrame;
 
 import java.awt.*;
 
